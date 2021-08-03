a7647e826e09d1e5edfcf596ce2e7ee391de769a
wil <wilsonaux1@gmail.com>
2021-08-03 12:41:49 -0500

    adicion de otros servicios

--- buscador-dni.py
+++ buscador-dni.py
@@ -3,7 +3,11 @@ from flask_cors import CORS
 from flask import Flask, request, jsonify
 import requests as http
 import json
+from dotenv import load_dotenv
+import os
+import apis
 
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -13,34 +17,5 @@ CORS(app)
 def default_main(dni):
     #  if request.args.get('dni'):
     if dni:
-        #  dni = request.args.get('dni')
-        #  r = http.get(f'https://api.apis.net.pe/v1/dni?numero=40180182')
-
-        try:
-            #  dni = '01784547'
-            token = 'apis-token-704.-xNqHkxR3NWNMFFRUASsH8aRfkSgCNz2'
-            result = http.get(f'https://api.apis.net.pe/v1/dni?numero={dni}', {
-                "headers": {
-                    "Authorization": "Bearer {token}"
-                    }
-                })
-            #  jsonedResponse = json.loads(result.text)
-            jsonedResponse = result.json()
-            nombreContribuyente = jsonedResponse['nombre'].split(' ')
-            nombreFinal = jsonedResponse['nombre']
-            if len(nombreContribuyente) >= 3:
-                nombres = ' '.join(nombreContribuyente[2:])
-                nombreFinal = f'{nombres} {nombreContribuyente[0]} {nombreContribuyente[1]}'
-            responseData = {
-                'nombre': nombreFinal,
-                'apellidos_nombres': jsonedResponse['nombre'],
-                'numeroDocumento': dni
-            }
-            return jsonify(responseData)
-            #  return jsonify(jsonedResponse)
-        except:
-            return jsonify({
-                'message': 'No lo encontre :/',
-                'code': '1'
-            }), 404
+        return apis.apifacturacion(dni, token=os.getenv('API_FACTURACION_TOKEN'))
     return ''
